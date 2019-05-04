c24379730eb102c7a187ca5aa772a2d3458b8b91
sergaro <sergiogruseros@gmail.com>
2019-05-04 12:36:04 +0200

    packaging changed

---
+++ HostAbroadTests/src/test/com/unitTests/TPlaceTest.java
@@ -0,0 +1,46 @@
+package com.unitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.business.enums.FamilyUnit;
+import com.business.transfers.TPlace;
+
+
+
+public class TPlaceTest {
+	private TPlace myPlace;
+
+	@Test
+	public void test() {
+		ArrayList<Date> noAvaliableDates = new ArrayList<Date>();
+		noAvaliableDates.add(new Date());
+		this.myPlace = new TPlace("calle piruleta", "dulce", noAvaliableDates, "foto", FamilyUnit.Alone, "Adri");
+		
+		TPlace aux = new TPlace(null, null, null, null, null, null);
+		
+		aux.setAddress("calle piruleta");
+		assertEquals(aux.getAddress(), this.myPlace.getAddress());
+		
+		aux.setDescription("dulce");
+		assertEquals(aux.getDescription(), this.myPlace.getDescription());
+		
+		aux.setNoAvaliableDates(noAvaliableDates);
+		assertEquals(aux.getNoAvaliableDates(), this.myPlace.getNoAvaliableDates());
+		
+		aux.setPhoto("foto");
+		assertEquals(aux.getPhoto(), this.myPlace.getPhoto());
+		
+		aux.setFamilyUnit(FamilyUnit.Alone);
+		assertEquals(aux.getFamilyUnit(), this.myPlace.getFamilyUnit());
+		
+		aux.setNickname("Adri");
+		assertEquals(aux.getNickname(), this.myPlace.getNickname());
+		
+	}
+
+}
---
+++ HostAbroadTests/src/test/com/unitTests/TUserTestHU34.java
@@ -0,0 +1,61 @@
+package com.unitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.business.transfers.TUser;
+
+public class TUserTestHU34 {
+	private TUser myUser;
+	
+	@Test
+	public void test() {
+		TUser aux;
+		ArrayList<String> likes = new ArrayList<>();
+		likes.add("a");
+		ArrayList<String> rates = new ArrayList<>();
+		rates.add("a");
+		
+		this.myUser = new TUser("Adri", "Adrian", "adri@gmail.com", "adri1", 5, "usuario viajero", true, false, likes, rates, null);
+		
+		aux = new TUser("Jose", "j", "j@a", "j1", 2, "usuario anfitrion", true, false, null, likes, rates);
+		
+		
+		aux.setNickname("Adri");
+		assertEquals(aux.getNickname(), this.myUser.getNickname());
+		
+		aux.setFullName("Adrian");
+		assertEquals(aux.getFullName(), this.myUser.getFullName());
+		
+		aux.setEmail("adri@gmail.com");
+		assertEquals(aux.getEmail(), this.myUser.getEmail());
+		
+		aux.setPassword("adri1");
+		assertEquals(aux.getPassword(), this.myUser.getPassword());
+		
+		aux.setRating(5);
+		assertEquals(aux.getRating(), this.myUser.getRating(), 2);
+		
+		aux.setDescription("usuario viajero");
+		assertEquals(aux.getDescription(), this.myUser.getDescription());
+		
+		aux.setHost(true);
+		assertEquals(aux.getHost(), this.myUser.getHost());
+		
+		aux.setTraveler(false);
+		assertEquals(aux.getTraveler(), this.myUser.getTraveler());
+		
+		aux.setLikes(likes);
+		assertEquals(aux.getLikes(), this.myUser.getLikes());
+		
+		aux.setMatches(null);
+		assertEquals(aux.getMatches(), this.myUser.getMatches());
+		
+		aux.setRates(rates);
+		assertEquals(aux.getRates(), this.myUser.getRates());
+	}
+
+}
